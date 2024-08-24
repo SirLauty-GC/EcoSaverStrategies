ef8e217d2b5909cdaf1ea6e6205de4395aefa930
Matt_qz <88900756+Matt-4-Code@users.noreply.github.com>
2024-08-23 21:25:03 -0300

    Preguntas frecuentes completas

--- src/proyectos/components/PreguntasFrecuentes.jsx
+++ src/proyectos/components/PreguntasFrecuentes.jsx
@@ -10,16 +10,16 @@ const PreguntasFrecuentes = () => {
                         <input type='checkbox' id='preguntaCollapsibleHeadOne'></input>
                         <label htmlFor='preguntaCollapsibleHeadOne' className='preguntaCollapsibleHeadOne'>¿Cómo crearon el sitio?</label>
                         <div className='preguntaRespuesta'>
-                            <p>Las tazas sobre el mantel, la lluvia derramada. Un poco de miel, un poco de miel, no basta...</p>
+                            <p>Utilizamos varias tecnologias del momento que convinamos para poder crear nuestra landing page entre el equipo de developers, entre las tecnologias utilizadas se encuentran: TaildWindCSS, React, Node.js, Github</p>
                         </div>
                     </div>
                 </div>
                 <div className='preguntaWrapper'>
                     <div className='preguntaCollapsible'>
                         <input type='checkbox' id='preguntaCollapsibleHeadTwo'></input>
-                        <label htmlFor='preguntaCollapsibleHeadTwo' className='preguntaCollapsibleHeadTwo'>¿Los productos son eco-amigables?</label>
+                        <label htmlFor='preguntaCollapsibleHeadTwo' className='preguntaCollapsibleHeadTwo'>¿Cuál fue la principal motivación detrás de la creación de este proyecto ecoamigable?</label>
                         <div className='preguntaRespuesta'>
-                            <p>Buena suerte y más que suerte, sin alarma. Me voy corriendo a ver qué escribe en mi pared la tribu de tu calle, la banda de mi calle.</p>
+                            <p>La motivacion se presento desde que nos dimos cuenta de lo desorbitado que puede ser el gasto inconciente de la energia, y lo que puede afectarles a las personas que lo derrochan y a nuestro medio ambiente.</p>
                         </div>
                     </div>
                 </div>
@@ -28,16 +28,16 @@ const PreguntasFrecuentes = () => {
                         <input type='checkbox' id='preguntaCollapsibleHeadThree'></input>
                         <label htmlFor='preguntaCollapsibleHeadThree' className='preguntaCollapsibleHeadThree'>¿Cuál es el objetivo?</label>
                         <div className='preguntaRespuesta'>
-                            <p>Vos poné la gasolina que yo prendo el motor ¿Podrás cruzar el puente sin caer? Al llegar la medianoche, aunque tenga un ala rota, voy a hacer mi tiro sin fallar.</p>
+                            <p>El fin de nuestro proyecto es crear un habito mas sustentable del uso de la energia con nuestros equipos de mediciones y calculos.</p>
                         </div>
                     </div>
                 </div>
                 <div className='preguntaWrapper'>
                     <div className='preguntaCollapsible'>
                         <input type='checkbox' id='preguntaCollapsibleHeadFour'></input>
-                        <label htmlFor='preguntaCollapsibleHeadFour' className='preguntaCollapsibleHeadFour'>¿Bover o Rica?</label>
+                        <label htmlFor='preguntaCollapsibleHeadFour' className='preguntaCollapsibleHeadFour'>¿Como trabajan nuestros equipos de developers en conjunto?</label>
                         <div className='preguntaRespuesta'>
-                            <p>Ángel de la soledad y de la desolación, preso de tu ilusión vas a bailar a bailar, bailar.</p>
+                            <p>Nuestros desarrolladores se encuentra separado en varios sectores con tareas especificas, para maximizar la eficiencia de trabajo ante tareas de alta complejidad</p>
                         </div>
                     </div>
                 </div>
