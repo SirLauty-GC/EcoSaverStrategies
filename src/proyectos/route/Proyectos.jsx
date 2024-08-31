4e086a397fcbaf7a6f89aa7f1b8e5e537c703842
Matt_qz <88900756+Matt-4-Code@users.noreply.github.com>
2024-08-31 09:30:47 -0300

    espaciado

--- src/proyectos/route/Proyectos.jsx
+++ src/proyectos/route/Proyectos.jsx
@@ -9,8 +9,12 @@ function proyectos() {
                 <h3 className='text-3xl font-medium text-emerald-700'>Proyectos anteriores:</h3>
             </div>
             <div className='pt-4 grid sm:flex justify-around'>
-                <ProyectoFigura nombre='DoctorStock'/>
-                <ProyectoFigura nombre='SensArInclusive'/>
+                <div>
+                    <ProyectoFigura nombre='DoctorStock'/>
+                </div>
+                <div className='pt-8 sm:pt-0 '>
+                    <ProyectoFigura nombre='SensArInclusive'/>
+                </div>
             </div>
             <div className='pt-4'>
                 <h3 className='text-3xl font-medium text-emerald-700'>Preguntas Frecuentes:</h3>
